50372d2d40f36b5897a84005ef5c4123cac6428c
scandalouss <dking3211@gmail.com>
2024-07-11 19:19:14 -0500

    super cleaned up the config

--- wezterm.lua
+++ wezterm.lua
@@ -1,7 +1,4 @@
---gonna clean this up hella later,
---just getting it to a working state atm
---it'll grow/get cleaned as necessary 
---or not, i'm not that picky
+--finally cleaned it up a bit
 
 local wezterm=require("wezterm")
 local config={}
@@ -10,87 +7,51 @@ if wezterm.config_builder then
     config=wezterm.config_builder()
 end
 
-config={
-    font=wezterm.font("Ubuntu Mono"),
-    font_size=11.0,
-    check_for_updates=true,
-    color_scheme="jellyfish",
-    window_background_opacity=0.95,
-    text_background_opacity=0.95,
-    hide_tab_bar_if_only_one_tab=true,
-    adjust_window_size_when_changing_font_size=false,
-    use_fancy_tab_bar=false,
-    front_end="OpenGL",
-    freetype_load_target="Light",
-    freetype_load_flags="NO_HINTING",
-    display_pixel_geometry="RGB",
-    warn_about_missing_glyphs=false,
-    enable_kitty_graphics=true,
-    default_cwd=wezterm.home_dir,
+--generic settings
+config.check_for_updates=true
+config.color_scheme="sleepyhollow"
+config.front_end="OpenGL"
+config.default_cwd=wezterm.home_dir
+config.enable_kitty_graphics=true
+config.window_background_opacity=0.95
+config.text_background_opacity=0.95
+config.use_fancy_tab_bar=false
+config.hide_tab_bar_if_only_one_tab=true
 
-    window_padding={
-        left=0,
-        right=0,
-        top=0,
-        bottom=0,
-    },
+--font stuff
+config.font=wezterm.font("Ubuntu Mono")
+config.font_size=12.0
+config.freetype_load_target="Light"
+config.freetype_load_flags="NO_HINTING"
+config.display_pixel_geometry="RGB"
+config.warn_about_missing_glyphs=true
+config.adjust_window_size_when_changing_font_size=false
 
-    --remove copy on selection/highlight, shit's annoying >:(
-    mouse_bindings={
-        {
-        event = { Up = { streak = 1, button = "Left" } },
-        mods = "NONE",
-        action = wezterm.action.Nop,
-    }
-    },
+config.window_padding={
+    left=0,
+    right=0,
+    top=0,
+    bottom=0
+}
 
-    keys={
-        {
-            key='LeftArrow',
-            mods='SHIFT',
-            action=wezterm.action.ActivateTabRelative(-1),
-        },
-        {
-            key='RightArrow',
-            mods='SHIFT',
-            action=wezterm.action.ActivateTabRelative(1),
-        },
-        {
-            key='j',
-            mods='CTRL|SHIFT',
-            action=wezterm.action.SplitHorizontal{
-                args={'bash'}
-            },
-        },
-        {
-            key='h',
-            mods='CTRL|SHIFT',
-            action=wezterm.action.SplitVertical{
-                args={'bash'}
-            },
-        },
-        {
-            key='LeftArrow',
-            mods='CTRL',
-            action=wezterm.action.ActivatePaneDirection 'Left',
-        },
-        {
-            key='RightArrow',
-            mods='CTRL',
-            action=wezterm.action.ActivatePaneDirection 'Right',
-        },
-        {
-            key='UpArrow',
-            mods='CTRL',
-            action=wezterm.action.ActivatePaneDirection 'Up',
+--keybinds
+config.mouse_bindings={
+    --get rid of the stupid copy on selection/highlight shit
+    {event={Up={streak=1,button="Left"}},mods="NONE",action=wezterm.action.Nop}
+}
 
-        },
-        {
-            key='DownArrow',
-            mods='CTRL',
-            action=wezterm.action.ActivatePaneDirection 'Down'
-        },
-    }
+config.keys={
+    --switching tabs
+    {key="LeftArrow",mods="SHIFT",action=wezterm.action.ActivateTabRelative(-1)},
+    {key="RightArrow",mods="SHIFT",action=wezterm.action.ActivateTabRelative(1)},
+
+    --creating and moving around panes
+    {key="j",mods="CTRL|SHIFT",action=wezterm.action.SplitHorizontal{args={"bash"}}},
+    {key="h",mods="CTRL|SHIFT",action=wezterm.action.SplitVertical{args={"bash"}}},
+    {key="LeftArrow",mods="CTRL",action=wezterm.action.ActivatePaneDirection("Left")},
+    {key="RightArrow",mods="CTRL",action=wezterm.action.ActivatePaneDirection("Right")},
+    {key="UpArrow",mods="CTRL",action=wezterm.action.ActivatePaneDirection("Up")},
+    {key="DownArrow",mods="CTRL",action=wezterm.action.ActivatePaneDirection("Down")},
 }
 
 return config
